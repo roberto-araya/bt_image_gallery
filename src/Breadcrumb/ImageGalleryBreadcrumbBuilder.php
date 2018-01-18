12590c04baeb61791b36589cde18dd376f5d6724
Roberto Araya <roberto.araya@btester.cl>
2018-01-18 12:52:33 -0300

    Issue #2937758 by roberto_araya: Clean code and apply coding standard

--- src/Breadcrumb/ImageGalleryBreadcrumbBuilder.php
+++ src/Breadcrumb/ImageGalleryBreadcrumbBuilder.php
@@ -4,92 +4,65 @@ namespace Drupal\bt_image_gallery\Breadcrumb;
 
 use Drupal\Core\Breadcrumb\Breadcrumb;
 use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
-use Drupal\Core\Session\AccountProxy;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Link;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\user\Entity\User;
+use Drupal\Core\Config\ConfigFactory;
 
+/**
+ * Class ImageGalleryBreadcrumbBuilder.
+ */
 class ImageGalleryBreadcrumbBuilder implements BreadcrumbBuilderInterface {
 
-    /**
-     * @var AccountInterface
-     */
-    protected $account;
+  /**
+   * The site name.
+   *
+   * @var string
+   */
+  protected $siteName;
 
-    /**
-     * The routes that will change their breadcrumbs.
-     */
-    private $routes = array(
-        //'entity.node.edit_form',
-        //'node.add',
-        'page_manager.page_view_bt_add_gallery',
-        'page_manager.page_view_bt_add_gallery_bt_add_gallery-panels_variant-0'
-        //'page_manager.page_view_bt_add_image',
-        //'page_manager.page_view_bt_add_slideshow',
-        //'page_manager.page_view_bt_add_video',
-        //'node.add_page',
-        //'entity.media.add_page',
-        //'entity.media.edit_form',
-        //'bt_add_block',
-        //'block_content.add_form',
-        //'page_manager.page_view_app_app-panels_variant-0',
-        //'page_manager.page_view_app_website_app_website-panels_variant-0',
-        //'page_manager.page_view_app_website_content_app_website_content-panels_variant-0',
-        //'page_manager.page_view_app_website_content_app_website_content-panels_variant-1',
-        //'page_manager.page_view_app_website_comments',
-        //'page_manager.page_view_app_website_documents_app_website_documents-panels_variant-0',
-        //'page_manager.page_view_app_website_documents_app_website_documents-panels_variant-1',
-        //'page_manager.page_view_app_website_media_app_website_media-panels_variant-0',
-        //'page_manager.page_view_app_website_media_app_website_media-panels_variant-1',
-        //'page_manager.page_view_app_website_polls_app_website_polls-panels_variant-0',
-        //'page_manager.page_view_app_website_foros_app_website_foros-panels_variant-0',
-        //'page_manager.page_view_app_website_blocks_app_website_blocks-panels_variant-0',
-        //'poll.poll_add',
-        //'entity.poll.edit_form',
-        //'bt_page_drag_and_drop',
-        //'page_manager.page_view_bt_upload_document',
-        //'bt_create_ipe_page',
-        //'bt_edit_ipe_page',
-        //'bt_delete_ipe_page',
-    );
+  /**
+   * The routes that will change their breadcrumbs.
+   *
+   * @var array
+   */
+  private $routes = array(
+    'page_manager.page_view_bt_add_gallery',
+    'page_manager.page_view_bt_add_gallery_bt_add_gallery-panels_variant-0',
+  );
 
-    /**
-     * Class constructor.
-     */
-    public function __construct(AccountProxy $current_user) {
-        $user_id = $current_user->id();
-        $user_account = User::load($user_id);
-        $this->account = $user_account;
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ConfigFactory $configFactory) {
+    $this->siteName = $configFactory->get('system.site')->get('name');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $attributes) {
+    $match = $this->routes;
+    if (in_array($attributes->getRouteName(), $match)) {
+      return TRUE;
     }
-
-    /**
-    * {@inheritdoc}
-    */
-    public function applies(RouteMatchInterface $attributes) {
-        $match = $this->routes;
-        if (in_array($attributes->getRouteName(),$match)) {
-            return TRUE;
-        }else{
-            return FALSE;
-        }
+    else {
+      return FALSE;
     }
+  }
 
-    /**
-    * {@inheritdoc}
-    */
-    public function build(RouteMatchInterface $route_match) {
-        $route = $route_match->getRouteName();
-        $breadcrumb = new Breadcrumb();
-        $breadcrumb->addCacheContexts(["url"]);
-        $site_name = \Drupal::config('system.site')->get('name');
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addCacheContexts(["url"]);
 
-        $breadcrumb->addLink(Link::createFromRoute($site_name, 'page_manager.page_view_app_app-panels_variant-0'));
-        $breadcrumb->addLink(Link::createFromRoute('Website', 'page_manager.page_view_app_website_app_website-panels_variant-0'));
-        $breadcrumb->addLink(Link::createFromRoute('Multimedia', 'page_manager.page_view_app_website_media_app_website_media-panels_variant-0'));
-        $breadcrumb->addLink(Link::createFromRoute('Add Multimedia Element', 'entity.media.add_page'));
+    $breadcrumb->addLink(Link::createFromRoute($this->siteName, 'page_manager.page_view_app_app-panels_variant-0'));
+    $breadcrumb->addLink(Link::createFromRoute('Website', 'page_manager.page_view_app_website_app_website-panels_variant-0'));
+    $breadcrumb->addLink(Link::createFromRoute('Multimedia', 'page_manager.page_view_app_website_media_app_website_media-panels_variant-0'));
+    $breadcrumb->addLink(Link::createFromRoute('Add Multimedia Element', 'entity.media.add_page'));
 
-        return $breadcrumb;
-    }
+    return $breadcrumb;
+  }
+
 }
--- src/Config/ConfigImageGalleryOverride.php
+++ src/Config/ConfigImageGalleryOverride.php
@@ -1,7 +1,4 @@
 <?php
-/**
- *
- */
 
 namespace Drupal\bt_image_gallery\Config;
 
@@ -14,57 +11,63 @@ use Drupal\Core\Config\ConfigFactory;
  */
 class ConfigImageGalleryOverride implements ConfigFactoryOverrideInterface {
 
-    private $views_admin_media;
-    private $views_full_admin_media;
+  private $viewsAdminMedia;
+  private $viewsFullAdminMedia;
 
-    public function __construct(ConfigFactory $ConfigFactory)
-    {
-        $this->views_admin_media = $ConfigFactory->get('views.view.bt_admin_multimedia');
-        $this->views_full_admin_media = $ConfigFactory->get('views.view.bt_full_admin_multimedia');
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ConfigFactory $configFactory) {
+    $this->viewsAdminMedia = $configFactory->get('views.view.bt_admin_multimedia');
+    $this->viewsFullAdminMedia = $configFactory->get('views.view.bt_full_admin_multimedia');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadOverrides($names) {
+    $overrides = array();
+
+    $media_values = [
+      'bt_image_gallery' => 'bt_image_gallery',
+    ];
+    // Add documents filter values to views.view.bt_admin_media view.
+    if (in_array('views.view.bt_admin_media', $names)) {
+      $views = $this->viewsAdminMedia;
+      $filter_values = $views->get('display.default.display_options.filters.bundle.value');
+      $values = array_merge($filter_values, $media_values);
+      $overrides['views.view.bt_admin_content']['display']['default']['display_options']['filters']['bundle']['value'] = $values;
+    }
+    // Add documents filter values to views.view.bt_full_admin_media view.
+    if (in_array('views.view.bt_full_admin_media', $names)) {
+      $views = $this->viewsFullAdminMedia;
+      $filter_values = $views->get('display.default.display_options.filters.type.value');
+      $values = array_merge($filter_values, $media_values);
+      $overrides['views.view.bt_full_admin_content']['display']['default']['display_options']['filters']['bundle']['value'] = $values;
     }
 
-    public function loadOverrides($names) {
-        $overrides = array();
+    return $overrides;
+  }
 
-        $media_values = [
-            'bt_image_gallery' => 'bt_image_gallery'
-        ];
-        // Add documents filter values to views.view.bt_admin_media view.
-        if (in_array('views.view.bt_admin_media', $names)) {
-            $views = $this->views_admin_media;
-            $filter_values = $views->get('display.default.display_options.filters.bundle.value');
-            $values = array_merge($filter_values,$media_values);
-            $overrides['views.view.bt_admin_content']['display']['default']['display_options']['filters']['bundle']['value'] = $values;
-        }
-        // Add documents filter values to views.view.bt_full_admin_media view.
-        if (in_array('views.view.bt_full_admin_media', $names)) {
-            $views = $this->views_full_admin_media;
-            $filter_values = $views->get('display.default.display_options.filters.type.value');
-            $values = array_merge($filter_values,$media_values);
-            $overrides['views.view.bt_full_admin_content']['display']['default']['display_options']['filters']['bundle']['value'] = $values;
-        }
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return 'ConfigImageGalleryOverride';
+  }
 
-        return $overrides;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getCacheSuffix() {
-        return 'ConfigImageGalleryOverride';
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getCacheableMetadata($name) {
-        return new CacheableMetadata();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
-        return NULL;
-    }
 }
